c90ced1a25955c6c161b8c2e1eaf311dbaa8fc9c
Patricio Leon <patricio.lyono@gmail.com>
2022-08-28 21:09:03 -0400

    first angular proyect

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ListPersonComponent } from './components/list-person/list-person.component';
+import { CreatePersonsComponent } from './components/create-persons/create-persons.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListPersonComponent,
+    CreatePersonsComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/create-persons/create-persons.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-persons',
+  templateUrl: './create-persons.component.html',
+  styleUrls: ['./create-persons.component.css']
+})
+export class CreatePersonsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
